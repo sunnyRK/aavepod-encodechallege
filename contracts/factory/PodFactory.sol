d7dde0b21b1ba63a0a9990eec9376001296686ea
sunnyRK <radadiyasunny970@gmail.com.com>
2020-07-12 09:50:45 +0530

    Added factory contract

---
+++ contracts/factory/PodFactory.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.6.0;
+
+import "../aavepod.sol";
+
+contract PodFactory{
+    address[] public podAddress;
+        
+    function createPod(uint256 minimum, uint256 numstakers, uint256 timeStamp, address tokenAddress, address aaveAddress, string memory betName) public{
+        aavepod newPod = new aavepod(minimum, numstakers, timeStamp, tokenAddress, aaveAddress, msg.sender, betName);
+        podAddress.push(address(newPod));
+    }
+    
+    function getPods() public view returns (address[] memory){
+        return podAddress;
+    }
+}
